93dbb5b172e3f86fcba3978150c1b81d270c22ae
Aleka <alena-up@mobileffort.com>
2019-04-08 18:24:33 +0300

    Settings for the VPS hosting are updated.

---
+++ omaha_server/omaha_server/settings_vps.py
@@ -0,0 +1,8 @@
+import os
+
+from .settings_prod import *
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATIC_URL = '/static/'
+
