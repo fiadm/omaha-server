cb4325204d484baca90f754c08fa8066f1af8605
aleka <alena-up@mobileffort.com>
2019-04-10 07:55:12 -0500

    Configuration for files and host name are updated.

--- omaha_server/omaha_server/settings_prod.py
+++ omaha_server/omaha_server/settings_prod.py
@@ -4,4 +4,8 @@ from .settings_default import *
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+PUBLIC_READ_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 STATIC_URL = '/static/'
+
+HOST_NAME = '206.54.191.7'
+OMAHA_URL_PREFIX = 'http://206.54.191.7'
