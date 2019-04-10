c56785d7f52d0f804ae5a6dda99206ab1d96938c
aleka <alena-up@mobileffort.com>
2019-04-10 09:19:09 -0500

    CUP validation is enabled with generated production private key.

--- omaha_server/omaha_server/settings_prod.py
+++ omaha_server/omaha_server/settings_prod.py
@@ -11,3 +11,8 @@ OMAHA_URL_PREFIX = 'http://{}'.format(HOST_NAME)
 
 STATIC_URL = '{}/static/'.format(OMAHA_URL_PREFIX)
 MEDIA_URL = '{}/static/media/'.format(OMAHA_URL_PREFIX)
+
+CUP_REQUEST_VALIDATION = True
+CUP_PEM_KEYS = {
+    '9': '/srv/omaha/deploy/cup_private.pem'
+}
