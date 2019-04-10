9edfe8cd521ae2e55bb220b0b6764067a89ab2ec
Aleka <alena-up@mobileffort.com>
2019-04-10 16:12:37 +0300

    URLs configuration for static/media files is updated.

--- omaha_server/omaha_server/settings_prod.py
+++ omaha_server/omaha_server/settings_prod.py
@@ -5,7 +5,9 @@ from .settings_default import *
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 PUBLIC_READ_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-STATIC_URL = '/static/'
 
 HOST_NAME = '206.54.191.7'
-OMAHA_URL_PREFIX = 'http://206.54.191.7'
+OMAHA_URL_PREFIX = 'http://{}'.format(HOST_NAME)
+
+STATIC_URL = '{}/static/'.format(OMAHA_URL_PREFIX)
+MEDIA_URL = '{}/static/media/'.format(OMAHA_URL_PREFIX)
