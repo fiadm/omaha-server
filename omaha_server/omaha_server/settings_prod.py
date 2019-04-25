44f63aa8bba0c67c5bbc5be0eb45a01fc38f2737
Aleka <alena-up@mobileffort.com>
2019-04-25 18:02:54 +0300

    Omaha URL includes domain name instead of IP address.

--- omaha_server/omaha_server/settings_prod.py
+++ omaha_server/omaha_server/settings_prod.py
@@ -6,7 +6,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 PUBLIC_READ_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
-HOST_NAME = '206.54.191.7'
+HOST_NAME = 'desktop-updater.com'
 OMAHA_URL_PREFIX = 'http://{}'.format(HOST_NAME)
 
 STATIC_URL = '{}/static/'.format(OMAHA_URL_PREFIX)
